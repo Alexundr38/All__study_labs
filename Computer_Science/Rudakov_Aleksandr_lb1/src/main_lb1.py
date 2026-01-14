db22fdefc6b4264c5e2e0b78b3ac4b17e4fc75c0
Alexundr38 <4sunwarrior1@mail.ru>
2026-01-14 23:38:01 +0300

    add_computer_science_sem1

--- Computer_Science/Rudakov_Aleksandr_lb1/src/main_lb1.py
+++ Computer_Science/Rudakov_Aleksandr_lb1/src/main_lb1.py
@@ -1,77 +1,34 @@
-class Figure:
-    def __init__(self,perimeter,area,color):
-        if isinstance(perimeter,int) and perimeter>0 and isinstance(area,int) and area>0 and (color=='r' or color=='b' or color=='g'):
-            self.perimeter = perimeter
-            self.area = area
-            self.color = color
-        else:
-            raise ValueError('Invlaid value')
+import numpy as np
 
 
-class Polygon(Figure): #Наследуется от класса Figure
-    def __init__(self,perimeter,area,color,angle_count,equilateral,biggest_angle):
-        if isinstance(perimeter,int) and isinstance(area,int) and isinstance(angle_count,int) and isinstance(equilateral,bool) and isinstance(biggest_angle,int) and (equilateral==True or equilateral==False) and biggest_angle>0 and  perimeter>0 and area>0 and angle_count>2 and (color=='r' or color=='b' or color=='g'):
-            super().__init__(perimeter, area, color)
-            self.angle_count = angle_count
-            self.equilateral = equilateral
-            self.biggest_angle = biggest_angle
-        else:
-            raise ValueError('Invalid value')
-    def __str__(self):
-        return f'Polygon: Периметр {self.perimeter}, площадь {self.area}, цвет фигуры {self.color}, количество углов {self.angle_count}, равносторонний {self.equilateral}, самый большой угол {self.biggest_angle}.'
-    def __add__(self):
-        return self.perimeter+self.area
-    def __eq__(self,other):
-        return self.perimeter==other.perimeter and self.area==other.area and self.angle_count==other.angle_count
-
-
-
-
-class Circle(Figure):  # Наследуется от класса Figure
-    def __init__(self,perimeter,area,color,radius,diametr):
-        if isinstance(perimeter,int) and isinstance(area,int) and isinstance(radius,int) and isinstance(diametr,int) and perimeter>0 and area>0 and radius>0 and diametr==2*radius and (color=='r' or color=='b' or color=='g'):
-            super().__init__(perimeter, area, color)
-            self.radius = radius
-            self.diametr = diametr
-        else:
-            raise ValueError('Invalid value')
-    def __str__(self):
-        return f'Circle: Периметр {self.perimeter}, площадь {self.area}, цвет фигуры {self.color}, радиус {self.radius}, диаметр {self.diametr}.'
-    def __add__(self):
-        return self.perimeter+self.area
-    def __eq__(self,other):
-        return self.radius==other.radius
-
-
-
-class PolygonList(list):
-    def __init__(self,name):
-        self.name=name
-    def append(self,p_object):
-        if isinstance(p_object,Polygon):
-            super().append(p_object)
-        else:
-            raise TypeError(f'Invalid type {type(p_object)}')
-    def print_colors(self):
-        counts=1
-        for item in self:
-            print(f'{counts} многоугольник: {item.color}')
-            counts+=1
-    def print_count(self):
-        print(len(self))
-
-
-
-class CircleList(list):
-    def __init__(self, name):
-        self.name=name
-    def extend(self,iterable):
-        super().extend(list(filter(lambda item: isinstance(item,Circle),iterable)))
-    def print_colors(self):
-        counts=1
-        for item in self:
-            print(f'{counts} окружность: {item.color}')
-            counts+=1
-    def total_area(self):
-        print(sum(item.area for item in self))
-
+def check_collision(bot1, bot2):
+    nbot1=list(bot1)
+    nbot2=list(bot2)
+    matr=np.array([[nbot1[0],nbot1[1]],[nbot2[0],nbot2[1]]])
+    ans=np.array([-nbot1[2],-nbot2[2]])
+    if np.linalg.matrix_rank(matr)<2:
+        return None
+    else:
+        answer=tuple(np.round(np.linalg.solve(matr,ans),2))
+        return answer
+        
+        
+def check_surface(point1, point2, point3):
+    npoint1=list(point1)
+    npoint2=list(point2)
+    npoint3=list(point3)
+    matr=np.array([[npoint1[0],npoint1[1],1],[npoint2[0],npoint2[1],1],[npoint3[0],npoint3[1],1]])
+    ans=np.array([npoint1[2],npoint2[2],npoint3[2]])
+    if np.linalg.matrix_rank(matr)<3:
+        return None
+    else:
+        answer=np.round(np.linalg.solve(matr,ans),2)
+        return answer
+        
+        
+def check_rotation(vec, rad):
+    c=np.cos(rad)
+    s=np.sin(rad)
+    round_z=np.array([[c,-s,0],[s,c,0],[0,0,1]])
+    answer=np.round(round_z.dot(vec),2)
+    return answer
